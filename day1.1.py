421b740144e4a810158a17dc6dad832815e964ca
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2024-12-02 13:49:58 +0300

    first commit

---
+++ day1.1.py
@@ -0,0 +1,37 @@
+import itertools 
+ 
+
+
+def distance():
+    with open("input.txt") as file:
+        data = file.readlines()
+    
+    digitOne = []
+    digitTwo = []
+
+    for datas in data:
+        nums = datas.strip().split()
+        digitOne.append(nums[0])
+        digitTwo.append(nums[1])
+
+    sortedOne = []
+    sortedTwo = []
+    diff = []
+    dist = 0
+    
+    sortedOne = sorted(digitOne)
+    sortedTwo = sorted(digitTwo)
+
+    for i,j in zip(sortedOne, sortedTwo):
+        diff.append(abs(int(i) - int(j)))
+        dist = sum(diff)
+        
+    print(dist)
+        
+
+
+def main():
+    distance()
+
+if __name__ == "__main__":
+    main()
